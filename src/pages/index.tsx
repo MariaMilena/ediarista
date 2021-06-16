71dc0295a069ecf5056be0f477ff3c0c93cedb7b
Milena <milena20182018@gmail.com>
2021-06-16 02:08:29 -0300

    Implementando o TextField

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
+import { Button, Typography } from '@material-ui/core';
+import { FormElementsContainer } from 'ui/styles/pages/index.style';
 
 export default function Home() {
   return (
@@ -10,6 +13,12 @@ export default function Home() {
         title={'Conheça os profissionais'}
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
       />
+      <FormElementsContainer>
+        <TextFieldMask mask={'99.999-999'} label={'Digite seu CEP'} fullWidth variant={'outlined'}/>
+        <Typography color={'error'}>CEP inválido</Typography>
+        <Button variant={'contained'} color={'secondary'} sx={{width: '200px'}}>Buscar</Button>
+      </FormElementsContainer>
+      
       <UserInformation 
         name={'Maria Milena'}
         picture={'https://github.com/MariaMilena.png'}
