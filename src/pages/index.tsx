6c3c20419167647bc2077adc6be57c383e1e907d
Milena <milena20182018@gmail.com>
2021-06-18 02:18:46 -0300

    Finalizando estilização dos containers de diaristas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,65 @@ import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
-import { Button, Typography } from '@material-ui/core';
-import { FormElementsContainer } from 'ui/styles/pages/index.style';
+import { Button, Typography, Container } from '@material-ui/core';
+import { FormElementsContainer, ProfissionaisPaper, ProfissionaisContainer } from 'ui/styles/pages/index.style';
 
 export default function Home() {
-  return (
+  return ( // O container serviu para centralizar os elementos 
     <div>
       <SafeEnvironment/>
       <PageTitle 
         title={'Conheça os profissionais'}
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
       />
-      <FormElementsContainer>
-        <TextFieldMask mask={'99.999-999'} label={'Digite seu CEP'} fullWidth variant={'outlined'}/>
-        <Typography color={'error'}>CEP inválido</Typography>
-        <Button variant={'contained'} color={'secondary'} sx={{width: '200px'}}>Buscar</Button>
-      </FormElementsContainer>
-      
-      <UserInformation 
-        name={'Maria Milena'}
-        picture={'https://github.com/MariaMilena.png'}
-        rating={3}
-        description={'Carira'}
-      />
+      <Container> 
+        <FormElementsContainer>
+          <TextFieldMask mask={'99.999-999'} label={'Digite seu CEP'} fullWidth variant={'outlined'}/>
+          <Typography color={'error'}>CEP inválido</Typography>
+          <Button variant={'contained'} color={'secondary'} sx={{width: '200px'}}>Buscar</Button>
+        </FormElementsContainer>
+
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation 
+              name={'Maria Milena'}
+              picture={'https://github.com/MariaMilena.png'}
+              rating={3}
+              description={'Carira'}
+            />
+            <UserInformation 
+              name={'Maria Milena'}
+              picture={'https://github.com/MariaMilena.png'}
+              rating={3}
+              description={'Carira'}
+            />
+            <UserInformation 
+              name={'Maria Milena'}
+              picture={'https://github.com/MariaMilena.png'}
+              rating={3}
+              description={'Carira'}
+            />
+            <UserInformation 
+              name={'Maria Milena'}
+              picture={'https://github.com/MariaMilena.png'}
+              rating={3}
+              description={'Carira'}
+            />
+            <UserInformation 
+              name={'Maria Milena'}
+              picture={'https://github.com/MariaMilena.png'}
+              rating={3}
+              description={'Carira'}
+            />
+            <UserInformation 
+              name={'Maria Milena'}
+              picture={'https://github.com/MariaMilena.png'}
+              rating={3}
+              description={'Carira'}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
--- src/ui/styles/pages/index.style.tsx
+++ src/ui/styles/pages/index.style.tsx
@@ -1,3 +1,4 @@
+import { Paper } from '@material-ui/core';
 import { experimentalStyled as styled } from '@material-ui/core/styles';
 
 export const FormElementsContainer = styled('div')`
@@ -6,5 +7,35 @@ export const FormElementsContainer = styled('div')`
     align-items: center;
     gap: ${({theme}) => theme.spacing(5)};
     max-width: 650px;
-    margin: 0 auto ${({theme}) => theme.spacing(7)}; // 1° margens de cima, 2° margens laterais, 3° margens debaixo
+    margin: 0 auto ${({theme}) => theme.spacing(7)}; // 1° margens de cima, 2° margens laterais(auto = mesma margem em ambos os lados, ou seja, centralizado), 3° margens debaixo
+`;
+
+export const ProfissionaisPaper = styled(Paper)`
+    padding: ${({theme}) => theme.spacing(7)};
+    margin: 0 auto ${({theme}) => theme.spacing(10)};
+
+    ${({theme}) => theme.breakpoints.down('md')} {
+        &.MuiPaper-root{
+            padding: 0;
+            box-shadow: none;
+        }
+    }
+`;
+
+export const ProfissionaisContainer = styled('div')`
+    display: grid;
+    grid-template-columns: 1fr; // força com que os elementos fiquem em uma coluna, ou seja, pega o total disponível
+
+    ${({theme}) => theme.breakpoints.up('md')} {
+        grid-template-columns: repeat(2, 1fr);
+        gap: ${({theme}) => theme.spacing(6)};
+    }
+
+    ${({theme}) => theme.breakpoints.down('md')} {
+        margin-left: ${({theme}) => theme.spacing(-2)};
+        margin-right: ${({theme}) => theme.spacing(-2)};
+        > :nth-of-type(odd) { // Altera a cor apenas das posições ímpares
+            background-color: ${({theme}) => theme.palette.background.paper};
+        }
+    }
 `;
